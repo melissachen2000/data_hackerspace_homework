731a84a72de025269db97a5ddaa692a0e1f3bb73
melissachen2000 <31445526+melissachen2000@users.noreply.github.com>
2018-09-18 18:07:39 -0500

    finished problem 1

--- assignment1/hw1.py
+++ assignment1/hw1.py
@@ -9,7 +9,25 @@ import csv
 import numpy as np
 
 def histogram_times(filename):
-    pass
+    crashes_per_hour = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    with open(filename) as data:
+        csv_reader = csv.reader(data)
+        data_list = list(csv_reader)
+        for crash in data_list[1:]:
+            if crash[1]:
+                time = crash[1]
+                hour = ""
+                
+                for letter in time:
+                    if letter in "0123456789":
+                        hour += letter
+                        
+                hour = hour.zfill(4)
+                hour = hour[:2]
+                
+                crashes_per_hour[int(hour)] += 1
+                
+    return crashes_per_hour
 
 def weigh_pokemons(filename, weight):
     pass
